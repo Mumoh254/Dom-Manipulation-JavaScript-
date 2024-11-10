a644b2ed6f6defefdf95a97dfb2e909ddc4067dd
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2024-11-10 14:45:11 +0000

    DOM

---
+++ manipulation/index.js
@@ -0,0 +1,12 @@
+
+console.log("Hellow  world")
+
+alert("hellow   world")
+console.log("hello")
+let   parent   =   document.querySelector("title")
+console.log(parent)
+
+let   button  =   document.getElementById("btn")
+console.log(button)
+console.log(parent.firstChild)
+
